f361be64d30f98cd81fafae826bd9cf0bf8dc439
fraserxu <xvfeng123@gmail.com>
2015-01-04 19:10:04 +0800

    add transform to images

--- dist/components/ImageViewer.js
+++ dist/components/ImageViewer.js
@@ -29,7 +29,8 @@ var ImageViewer = React.createClass({
       x: imgElement.x,
       y: imgElement.y,
       width: imgElement.width,
-      height: imgElement.height
+      height: imgElement.height,
+      offsetTop: imgElement.getBoundingClientRect().top
     };
   },
 
@@ -39,14 +40,22 @@ var ImageViewer = React.createClass({
 
   render: function () {
     var transfromStyle;
-    var viewSrc = this.state.scaled ? "" + this.state.src + "?imageView/2/w/800/h/800" : "" + this.state.src + "?imageView/2/w/300/h/300";
+    var viewSrc = this.state.scaled ? "" + this.state.src + "?imageView/2/h/" + window.innerHeight : "" + this.state.src + "?imageView/2/w/300/h/300";
 
     if (this.state.scaled) {
-      var y = -this.state.initialPosition.y;
+      var y = (window.innerHeight - this.state.initialPosition.height) / 2 - this.state.initialPosition.offsetTop;
+      var x = (window.innerWidth - this.state.initialPosition.width) / 2 - this.state.initialPosition.x;
+      var scale = window.innerHeight / this.state.initialPosition.height;
       transfromStyle = {
-        WebkitTransform: "translate(-100px, " + y + "px) scale(1.5981018394199785)",
+        WebkitTransform: "translate(" + x + "px, " + y + "px) scale(" + scale + ")",
+        WebkitTransition: "all 0.6s",
         position: "relative",
         zIndex: 250
+      };
+    } else {
+      transfromStyle = {
+        WebkitTransform: "translate(0, 0) scale(1)",
+        WebkitTransition: "all 0.6s"
       };
     }
     return React.createElement("div", {
